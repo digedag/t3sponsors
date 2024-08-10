2b051b1abf14dda8bd59707b0cd0db7b1ddc12bb
René Nitzsche <rene@system25.de>
2024-08-10 18:12:55 +0200

    Remove class file

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,31 +5,28 @@ if (!(defined('TYPO3') || defined('TYPO3_MODE'))) {
 
 
 \Sys25\RnBase\Utility\Extensions::addService($_EXTKEY, 't3sponsors' /* sv type */, 'tx_t3sponsors_sv1_Sponsor' /* sv key */,
-    array(
+    [
         'title' => 'Sponsoren', 'description' => 'Handles sponsors', 'subtype' => 'sponsor',
         'available' => true, 'priority' => 50, 'quality' => 50,
         'os' => '', 'exec' => '',
-        'classFile' => \Sys25\RnBase\Utility\Extensions::extPath($_EXTKEY).'sv1/class.tx_t3sponsors_sv1_Sponsor.php',
         'className' => 'tx_t3sponsors_sv1_Sponsor',
-    )
+    ]
 );
 
 \Sys25\RnBase\Utility\Extensions::addService($_EXTKEY, 't3sponsors' /* sv type */, 'tx_t3sponsors_sv1_Category' /* sv key */,
-    array(
+    [
         'title' => 'Sponsoren', 'description' => 'Handles sponsors categories', 'subtype' => 'category',
         'available' => true, 'priority' => 50, 'quality' => 50,
         'os' => '', 'exec' => '',
-        'classFile' => \Sys25\RnBase\Utility\Extensions::extPath($_EXTKEY).'sv1/class.tx_t3sponsors_sv1_Category.php',
         'className' => 'tx_t3sponsors_sv1_Category',
-    )
+    ]
 );
 
 \Sys25\RnBase\Utility\Extensions::addService($_EXTKEY, 't3sponsors' /* sv type */, 'tx_t3sponsors_sv1_Trade' /* sv key */,
-    array(
+    [
         'title' => 'Sponsoring-Trades', 'description' => 'Handles trades', 'subtype' => 'trade',
         'available' => true, 'priority' => 50, 'quality' => 50,
         'os' => '', 'exec' => '',
-        'classFile' => \Sys25\RnBase\Utility\Extensions::extPath($_EXTKEY).'sv1/class.tx_t3sponsors_sv1_Trade.php',
         'className' => 'tx_t3sponsors_sv1_Trade',
-    )
+    ]
 );
